f1233261c51b6bcaf8f2dcf2bd9b039fd85e4547
Darbu <kostas.92@mail.ru>
2020-01-14 22:48:24 +0300

    com 1

--- WpfApp48/MainWindow.xaml.cs
+++ WpfApp48/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp48
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi!");
+        }
     }
 }
